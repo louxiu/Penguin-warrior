3b6d0f709765734d987e533e353b29d51c7cca54
louxiu <lou.0211@gmail.com>
2012-06-11 17:11:16 +0800

    Init the fighter project

---
+++ config.h
@@ -0,0 +1,32 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "fighter"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "fighter"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "fighter 1.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "fighter"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.1"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "1.1"
